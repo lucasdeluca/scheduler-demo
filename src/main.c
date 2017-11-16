2b58022329befce03fcb1e9c62597a31ef3ea877
lucasdeluca <delucca.lucas@bol.com.br>
2017-11-16 17:13:28 -0200

    codigo atualizado

--- src/main.c
+++ src/main.c
@@ -2,17 +2,17 @@
 
 int main() {
   int a[5];
-  int n;
+  int n, i, j;
   char posicao[5];
   char flag;
 
   scanf("%d %d %d %d %d %d", &(a[0]), &(a[1]), &(a[2]), &(a[3]), &(a[4]), &n);
   
-  for (int j=0; j<5; j++){
-  	posicao[j] = 1;
+  for (i=0; i<5; i++){
+  	posicao[i] = 1;
   }
 
-  for (int i=0; i<n; i++) {
+  for (j=0; j<n; j++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
   
 	if (a[0] == posicao[0]){
@@ -20,35 +20,30 @@ int main() {
 		flag = 1;
 		posicao[0] = 0;	
 	}
-	posicao[0] = posicao[0] + 1;
 
 	if (a[1] == posicao[1]){
 		printf("1");
 		flag = 1;
 		posicao[1] = 0;	
 	}
-	posicao[1] = posicao[1] + 1;
 
 	if (a[2] == posicao[2]){
 		printf("2");
 		flag = 1;
 		posicao[2] = 0;	
 	}
-	posicao[2] = posicao[2] + 1;
 
 	if (a[3] == posicao[3]){
 		printf("3");
 		flag = 1;
 		posicao[3] = 0;	
 	}
-	posicao[3] = posicao[3] + 1;
 
 	if (a[4] == posicao[4]){
 		printf("4");
 		flag = 1;
 		posicao[4] = 0;	
 	}
-	posicao[4] = posicao[4] + 1;
 
 	if (flag == 0){
 		printf("-");	
@@ -57,6 +52,10 @@ int main() {
 	printf("\n");
 
 	flag = 0;
+
+	for (i=0; i<5; i++){
+		posicao[i] = posicao[i] + 1;	
+	}
   }
 
   return 0;
