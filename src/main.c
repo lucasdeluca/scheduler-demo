c25309d51fbf611451544e9c32599efd134ddf49
lucasdeluca <delucca.lucas@bol.com.br>
2017-11-16 16:38:50 -0200

    problema resolvido

--- src/main.c
+++ src/main.c
@@ -15,46 +15,48 @@ int main() {
   for (int i=0; i<n; i++) {
     /* Imprime resultado da n-esima iteracao do scheduler */
   
-	if (a[0] = posicao[0]){
+	if (a[0] == posicao[0]){
 		printf("0");
 		flag = 1;
 		posicao[0] = 0;	
 	}
 	posicao[0]++;
 
-	if (a[1] = posicao[1]){
+	if (a[1] == posicao[1]){
 		printf("1");
 		flag = 1;
 		posicao[1] = 0;	
 	}
 	posicao[1]++;
 
-	if (a[2] = posicao[2]){
+	if (a[2] == posicao[2]){
 		printf("2");
 		flag = 1;
 		posicao[2] = 0;	
 	}
 	posicao[2]++;
 
-	if (a[3] = posicao[3]){
+	if (a[3] == posicao[3]){
 		printf("3");
 		flag = 1;
 		posicao[3] = 0;	
 	}
 	posicao[3]++;
 
-	if (a[4] = posicao[4]){
+	if (a[4] == posicao[4]){
 		printf("4");
 		flag = 1;
 		posicao[4] = 0;	
 	}
 	posicao[4]++;
 
-	if (flag = 0){
+	if (flag == 0){
 		printf("-");	
 	}
 
 	printf("\n");
+
+	flag = 0;
   }
 
   return 0;
